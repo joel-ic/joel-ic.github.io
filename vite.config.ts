6b0a3037f00a9e930fe8012b8f6fba84cd600bc7
Joel-IC <joel.ic2000@hotmail.com>
2024-12-03 21:58:25 -0500

    despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/joel-ic.github.io/' : '/'
 })
+
